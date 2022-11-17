eaf9eea82b9bd19a0264248cac41e2363b4dc709
Jakub Przybysz <kuba.kubis@interia.pl>
2022-11-17 23:38:56 +0100

    Create

--- app/src/main/java/com/przybysz/runapp/Register.java
+++ app/src/main/java/com/przybysz/runapp/Register.java
@@ -1,5 +1,6 @@
 package com.przybysz.runapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,8 +11,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity implements View.OnClickListener{
 
@@ -100,5 +106,29 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
         }
 
         progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()){
+                        User user = new User(fullName, email);
+                        FirebaseDatabase.getInstance().getReference("Users")
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(Register.this, "User has been registered successfully!", Toast.LENGTH_LONG).show();
+                                    progressBar.setVisibility(View.GONE);
+                                    startActivity(new Intent(Register.this, MainActivity.class));
+                                } else {
+                                    Toast.makeText(Register.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                                    progressBar.setVisibility(View.GONE);
+                                }
+                            }
+                        });
+                    } else {
+                        Toast.makeText(Register.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
     }
 }
